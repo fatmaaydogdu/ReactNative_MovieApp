f09f4ffdbef99671f0c65d602e60f240b1898d08
fatmaaydogdu <fatmaaydogdu581@gmail.com>
2023-11-19 18:19:32 +0300

    search screen api

--- screens/SearchScreen.js
+++ screens/SearchScreen.js
@@ -1,24 +1,47 @@
 import { View, Text, Dimensions, TextInput, TouchableOpacity, ScrollView, TouchableWithoutFeedback, Image } from 'react-native'
-import React, { useState } from 'react'
+import React, { useCallback, useState } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { XMarkIcon } from 'react-native-heroicons/outline';
 import { useNavigation } from '@react-navigation/native';
 import Loading from '../components/loading';
+import { debounce } from 'lodash';
+import {fallbackMoviePoster, image185, searchMovies} from '../api/moviedb';
 
 var { width, height } = Dimensions.get("window");
 
 export default function SearchScreen() {
 
   const navigation = useNavigation();
-  const [results, setResults] = useState([1, 2, 3, 4]);
+  const [results, setResults] = useState([]);
   const [loading, setLoading] = useState(false);
 
-  let movieName = "Five Nights at Freddy's"
+
+  const handleSearch = value => {
+    if(value && value.length>2){
+      setLoading(true);
+      searchMovies({
+        query: value,
+        include_adult: 'false',
+        language: 'en-US',
+        page: '1'
+      }).then(data=> {
+        setLoading(false);
+        if(data && data.results) setResults(data.results);
+
+      })
+    } else {
+      setLoading(false);
+      setResults([]);
+    }
+  }
+
+  const handleTextDebounce = useCallback(debounce(handleSearch, 400), []); 
 
   return (
     <SafeAreaView className="bg-neutral-800 flex-1">
       <View className="mx-4 mb-3 flex-row justify-between items-center border border-neutral-500 rounded-full">
         <TextInput
+          onChangeText={handleTextDebounce}
           placeholder='Search Movie'
           placeholderTextColor={'lightgray'}
           className="pb-1 pl-6 font-semibold text-white tracking-wider" />
@@ -50,11 +73,11 @@ export default function SearchScreen() {
                         >
                           <View className="space-y-2 mb-4">
                             <Image className="rounded-3xl"
-                              source={require('../assets/images/moviePoster2.jpg')}
+                              source={{uri: image185(item?.poster_path) || fallbackMoviePoster}}
                               style={{ width: width * 0.44, height: height * 0.3 }}
                             />
                             <Text className="text-neutral-300 ml-1">
-                              {movieName.length > 22 ? movieName.slice(0, 22) + '...' : movieName} </Text>
+                              {item?.title.length > 22 ? item?.title.slice(0, 22) + '...' : item?.title} </Text>
                           </View>
 
                         </TouchableWithoutFeedback>
